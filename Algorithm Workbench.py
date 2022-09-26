1f2f9e13d747e56a69d79e4a86ecb9092362e0a6
wfernandesneto <wfernandesneto.my.ccri.edu>
2022-09-26 19:31:25 -0400

    Tracking Commit Alogrithm

---
+++ Algorithm Workbench.py
@@ -0,0 +1,83 @@
+
+# #height=input("What's your height ? ")
+# #print("You are",height,".")
+#
+# print("")
+#
+# #fav_color=input("What's your favorite color ?")
+# #print("Your favorite color is",fav_color,".")
+#
+# print("")
+#
+#
+# #b=2+a         Adds 2 to a and assigns the result to b
+# #a=4*b         Multiplies b times 4 and assigns the result to a
+# #b=a/3.14      Divides a by 3.14 and assigns the result to b
+# #a=b-8         Subtracts 8 from b and assigns the result to a
+#
+# w=5
+# x=5
+# y=8
+# z=2
+#
+# print("result x + y =",x + y)
+# print("result z * 2 =",z * 2)
+# print("result y / x =",y/x)
+# print("result y – z =",y-z)
+# print("result w // z =",w//z)
+#
+# print("")
+#
+# n=10+14
+# print("n=",n)
+#
+# print("")
+# #variable_down_payment=float(input("variable down payment="))
+# #variable_total=float(input("variable total="))
+# #variable_due=(variable_total-variable_down_payment)
+#
+#
+# #print("variable due=",variable_due)
+#
+# total=subtotal*0.15
+#
+# # a = 5
+# # b = 2
+# # c = 3
+# # result = a + b * c
+# # print(result)
+#
+#
+# num = 99
+# num = 5
+# print(num)
+#
+# variable_sales=float(input)
+#
+#
+#
+#
+#
+#
+# print('Mon\tTue\tWed')
+# print('Thu\tFri\tSat')
+#
+# name="Wolfio"
+#
+# print(f"My name is {name}.")
+#
+#
+# num=155969.4455
+#
+# print(f"{num:.2f}") #prints out num with 2 decimal places
+
+number = 1234567.456
+print(f"{number:.2f}")
+
+print("")
+
+print(f"{number:7,.1f}")
+
+print("")
+
+print('George', 'John', 'Paul', 'Ringo', sep=' ')
